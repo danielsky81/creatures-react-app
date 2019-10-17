bc98a7155ee8e2dec7c10a10e838fcfbb4c0cc2d
danielsky <karma0rg4u@gmail.com>
2019-10-17 23:00:48 +0100

    Add styling to the app

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
         const filterCreatures = creatures.filter(creature => creature.name.toLowerCase().includes(searchField.toLowerCase()))
         return (
             <div className="App">
+            <h1>Robots Universum</h1>
                 <SearchBox
                     placeholder="search creatures"
                     handleChange={this.handleChange}
